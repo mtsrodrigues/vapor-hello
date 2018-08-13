972543b19f0d8c9bf0b44b8cdbf166b387c0c768
Mateus Rodrigues <mateus.fr26@gmail.com>
2018-08-12 22:33:10 -0300

    first commit

---
+++ Sources/App/configure.swift
@@ -0,0 +1,12 @@
+import Vapor
+
+public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
+
+    let router = EngineRouter.default()
+    try routes(router)
+    services.register(router, as: Router.self)
+
+    var middlewares = MiddlewareConfig()
+    services.register(middlewares)
+
+}
---
+++ Sources/App/routes.swift
@@ -0,0 +1,8 @@
+import Vapor
+
+public func routes(_ router: Router) throws {
+    
+    router.get("hello") { req in
+        return "Hello, world!"
+    }
+}
